7e7193e9588d74d7d68ca299ca06a9c8bb161fb5
Pedro Adorno <pedroadorno99@gmail.com>
2023-08-13 22:59:15 -0300

    makepkg permission test

--- roles/base/files/users/quatro/scripts/archcraft/build-packages.sh
+++ roles/base/files/users/quatro/scripts/archcraft/build-packages.sh
@@ -3,30 +3,36 @@
 ## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
 
 ## Script Termination
-exit_on_signal_SIGINT () {
-    { printf "\n\n%s\n" "Script interrupted." 2>&1; echo; }
-    exit 0
+exit_on_signal_SIGINT() {
+	{
+		printf "\n\n%s\n" "Script interrupted." 2>&1
+		echo
+	}
+	exit 0
 }
 
-exit_on_signal_SIGTERM () {
-    { printf "\n\n%s\n" "Script terminated." 2>&1; echo; }
-    exit 0
+exit_on_signal_SIGTERM() {
+	{
+		printf "\n\n%s\n" "Script terminated." 2>&1
+		echo
+	}
+	exit 0
 }
 
 trap exit_on_signal_SIGINT SIGINT
 trap exit_on_signal_SIGTERM SIGTERM
 
 # Build packages
-build_pkg () {
+build_pkg() {
 	echo -e "\nBuilding Package ${1} - \n"
 	cd ${1}
-  rm -r pkg src *.pkg.tar.zst
-  makepkg -sc
+	rm -r pkg src *.pkg.tar.zst
+	makepkg -scfi
 
 	if [[ "$1" == "archcraft-st" ]]; then
 		rm *.tar.gz
 	fi
-	
+
 	cd -
 }
 
